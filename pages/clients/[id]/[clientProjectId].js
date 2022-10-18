e974d1476a4cbccb05b8cbad2a4cbc844fd1a8ac
bskipper <bskipper@uabmc.edu>
2022-10-18 15:45:53 -0400

    Finished course 3

---
+++ pages/clients/[id]/[clientProjectId].js
@@ -0,0 +1,11 @@
+const SelectedClientProjectPage = () => {
+    return (
+        <div>
+            <h1>
+                The Selected Client Selected Project Page
+            </h1>
+        </div>
+    )
+}
+
+export default SelectedClientProjectPage
