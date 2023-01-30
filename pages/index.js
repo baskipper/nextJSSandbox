2e78c91ea8f34ce980552e8ba89176622e1c390b
bskipper <bskipper@uabmc.edu>
2023-01-30 11:15:04 -0500

    Finished course 7

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import Head from 'next/head'
+
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
 
 const HomePage = ({events}) => {
   return (
       <div>
+          <Head>
+              <title>NextJS Events</title>
+              <meta name='description' content='Find a lot of great events that allow you to evolve....' />
+          </Head>
         <EventList
             items={events}
         />
